e1d6cbee96a2a9b99ff97d6219a0e0244844137c
Jack Graves <Jack.Graves@automation-consultants.com>
2018-05-16 20:14:55 +0100

    Improved Stream Implementation, Improved Argument Handling, Custom Definition Files

--- README.md
+++ README.md
@@ -8,13 +8,13 @@ The aim is to increase the speed of scanning dramatically compared to executing 
 A 3MB Log File took the following duration:
 * Support Tools Log Analyser: 6 minutes 14 seconds
 * Standalone Log Scanner (sequential): 1 minute 48 seconds
-* Standalone Log Scanner (parrallel): 37 seconds
+* Standalone Log Scanner (parrallel): 34 seconds
 
-*That's an over 70% improvement when sequentially run and a 90% improvement when run in parrallel*
+*That's an over 70% improvement when sequentially run and an over 90% improvement when run in parrallel*
 
 ## How it Works
 The following steps are followed:
-1. (if it doesn't already exist) Download the definition file from the Atlassian website
+1. (if it doesn't already exist) Download the definition file from the Atlassian website, or a custom URL
 2. Parses the XML into JAXB Objects
 3. Generates Regular Expression List
 4. Reads the Log File
@@ -23,11 +23,18 @@ The following steps are followed:
 
 ## Compiling
 Run the following command to build the project into a JAR file:
+
 `mvn package`
 
 ## Usage
-Execute the following on your command line (CMD or Bash) and leave the "stream" parameter out if you would like it to run sequentially (which is much less demanding)
-`java -jar logscanner.jar "jira-core" "atlassian-jira.log" "stream"`
+Execute the following on your command line (CMD or Bash) and leave the "stream" parameter out if you would like it to run sequentially (which is much less demanding).
+
+`java -jar log-scanner.jar -def=jira-core -log=atlassian-jira.log -stream`
+
+## Custom Definitions
+This tool supports custom definitions, by providing a URL as the definition argument:
+
+`-def=https://www.example.com/definition.xml`
 
 ## Download
 You can download a pre-compiled binary from the [releases page](https://github.com/jackgraves/standalone-atlassian-log-scanner/releases)
@@ -35,11 +42,16 @@ You can download a pre-compiled binary from the [releases page](https://github.c
 ## Example
 ### Built-in Tool
 
-![Support Tools Output](example/example_output_supporttools.png)
+![Support Tools Output](example/screenshot-hercules.png)
 
 ### Standalone Tool
 
-![SALSA Output](example/example_output_standalone.png)
+![SALSA Output](example/screenshot-salsa.png)
 
-## Next Steps
-- [ ] Write Jira plugin to automatically scan incoming support log files
+## Improvements
+- [x] Use Parallel Streams API to speed up analysis
+- [x] Implement flags and arguments
+- [ ] Show line number/date against each detected problem
+- [ ] Implement multiple definitions per analysis (by combining XML)
+- [ ] Implement Jira Service Desk App for use with support tickets
+- [ ] Port to Node.js for use with NPM
--- src/main/java/co/uk/jackgraves/logscanner/LogScanner.java
+++ src/main/java/co/uk/jackgraves/logscanner/LogScanner.java
@@ -1,5 +1,6 @@
 package co.uk.jackgraves.logscanner;
 
+import co.uk.jackgraves.logscanner.options.Options;
 import co.uk.jackgraves.logscanner.xml.ObjectStream;
 import co.uk.jackgraves.logscanner.xml.RegExItem;
 
@@ -7,6 +8,7 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
@@ -14,19 +16,23 @@ import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.zip.CRC32;
 
 /**
- * Log Scanner for Atlassian Applications
+ * Log Scanner for Atlassian Applications.  Supports:
+ *  - Sequential and Parallel Modes
+ *  - Atlassian or Custom Definition File
  *
- * Arguments:   Product (jira-core, jira-soft, jira-desk, confluence, crowd, bitbucket)
- * Log File:    Location of Log File (e.g. atlassian-jira.log)
- * Use Streams: Use Java 8 Stream API. Faster, but uses more resources. "stream" or leave empty
+ * Arguments:
+ *  -def (Definitions) - 1 of jira-core | jira-soft | jira-desk | confluence | crowd | bitbucket or a custom definition URL
+ *  -log (Location of Log File) - e.g. atlassian-jira.log
+ *  -stream (Run in Parrallel) - flag (set/unset)
  *
- * e.g. java -jar logscanner.jar "jira-core" "atlassian-jira.log" "stream"
+ * e.g. java -jar log-scanner.jar -def=jira-core -log=atlassian-jira.log -stream
  *
  */
 @SuppressWarnings("ALL")
-public class Main {
+public class LogScanner {
     private static final String[] PRODUCT_DEFINITIONS = {
             "https://confluence.atlassian.com/support/files/179443532/792496554/2342/1525743696518/jira_regex_v2.xml",
             "https://confluence.atlassian.com/support/files/179443532/792496607/2364/1525741337514/greenhopper_regex_v2.xml",
@@ -36,48 +42,95 @@ public class Main {
             "https://confluence.atlassian.com/support/files/179443532/792303609/2314/1525744113860/stash_regex_v2.xml",
             "https://confluence.atlassian.com/support/files/179443532/792630874/2361/1525737651612/crowd_regex_v2.xml"
     };
-    
-    private static final String COMPLETE = "      Complete\r\n";
+    private static final String RETURN = "\r\n";
+    private static final String SPACING = "      ";
+    private static final String COMPLETE = SPACING + "Complete" + RETURN;
 
     public static void main(String[] args) {
+        // Initialise Variables
+        boolean stream = false;
+        String logFile = null;
+        String defInput = null;
+
+        // Options
+        Options opt = new Options(args, 2);
+        opt.getSet().addOption("stream", Options.Multiplicity.ZERO_OR_ONE);
+        opt.getSet().addOption("log", Options.Separator.EQUALS, Options.Multiplicity.ZERO_OR_ONE);
+        opt.getSet().addOption("def", Options.Separator.EQUALS, Options.Multiplicity.ZERO_OR_ONE);
+        opt.check();
+
+        // Respond to Options
+        if (opt.getSet().isSet("log")) {
+            logFile = opt.getSet().getOption("log").getResultValue(0);
+        } else {
+            print("Error: No Log File Specified (-log=xxx.log)" + RETURN);
+            print("Please specify a log file to analyse" + RETURN);
+            System.exit(1);
+        }
+        if (opt.getSet().isSet("def")) {
+            defInput = opt.getSet().getOption("def").getResultValue(0);
+        } else {
+            print("Error: No Definition Specified (-def=jira-core)" + RETURN);
+            print("Please specify [jira-core, jira-soft, jira-desk, confluence, bitbucket, bamboo, crowd] or a definition URL (http/https)" + RETURN);
+            System.exit(1);
+        }
+        if (opt.getSet().isSet("stream")) {
+            stream = true;
+        }
+
+        // Run Scanner
+        runScanner(defInput, logFile, stream);
+    }
+
+    private static void runScanner(String definition, String logFile, boolean stream) {
         // Introduction
-        print("[ Standalone Atlassian Log Scanner - Started ]\r\n\r\n");
+        String mode = "Sequential";
+        if(stream) {
+            mode = "Parrallel";
+        }
+        print("[ Standalone Atlassian Log Scanner - Started (" + mode + " Mode) ]" + RETURN + RETURN);
+
         // Download Definition
-        print("[1/5] Downloading Definitions... \r\n");
-        if(!new File(args[0] + ".xml").isFile()) {
-            downloadDefinition(args[0]);
+        print("[1/5] Downloading Definitions..." + RETURN);
+        if(!new File(getFileName(definition)).isFile()) {
+            downloadDefinition(definition);
             print(COMPLETE);
         } else {
-            print("      Skipping (Already Downloaded)\r\n");
+            print(SPACING + "Skipping (Already Downloaded)" + RETURN);
         }
+
         // Process XML
-        print("[2/5] Parsing XML... \r\n");
-        List<RegExItem> regexItems = Objects.requireNonNull(unmarshallXml(args[0])).regexItems;
+        print("[2/5] Parsing XML..." + RETURN);
+        List<RegExItem> regexItems = Objects.requireNonNull(unmarshallXml(definition).regexItems);
         print(COMPLETE);
+
         // Build RegEx List
-        print("[3/5] Generating Regular Expressions... \r\n");
+        print("[3/5] Generating Regular Expressions..." + RETURN);
         HashMap<String, Pattern> regularExpressions = new HashMap<>();
         for (RegExItem regexItem : regexItems) {
             regularExpressions.put(regexItem.URL, Pattern.compile(regexItem.regex));
         }
         print(COMPLETE);
+
         // Read Log File
-        print("[4/5] Reading Log File... \r\n");
-        ArrayList<String> logFile = readLogFile(args[1]);
+        print("[4/5] Reading Log File..." + RETURN);
+        ArrayList<String> log = readLogFile(logFile);
         print(COMPLETE);
+
         // Parse Logs
-        print("[5/5] Parsing Log Lines... \r\n");
+        print("[5/5] Parsing Log Lines..." + RETURN);
         ArrayList<String> errors;
-        if(args[2].equals("stream")) {
-            errors = parseLogStream(logFile, regularExpressions);
+        if(stream) {
+            errors = parseLogStream(log, regularExpressions);
         } else {
-            errors = parseLog(logFile, regularExpressions);
+            errors = parseLog(log, regularExpressions);
         }
-        print("\r      Complete\r\n");
+        print("\r" + COMPLETE);
+
         // Print Errors
-        print("\r\nDetected Problems:\r\n");
+        print(RETURN + "Detected Problems:" + RETURN);
         for(String url : errors) {
-            print("      " + url + "\r\n");
+            print("      " + url + RETURN);
         }
         print("\r\n[ Standalone Atlassian Log Scanner - Finished ]\r\n");
     }
@@ -95,23 +148,23 @@ public class Main {
         }
     }
 
-    private static void downloadDefinition(String product) {
+    private static void downloadDefinition(String defInput) {
         try {
-            URL website = new URL(getDefinitionUrl(product));
+            URL website = getUrl(defInput);
             ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-            FileOutputStream fos = new FileOutputStream(product + ".xml");
+            FileOutputStream fos = new FileOutputStream(getFileName(defInput));
             fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    private static ObjectStream unmarshallXml(String product) {
+    
+    private static ObjectStream unmarshallXml(String defInput) {
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance(ObjectStream.class);
             Unmarshaller jaxbUnmarshaller;
             jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            return (ObjectStream) jaxbUnmarshaller.unmarshal( new File(product + ".xml") );
+            return (ObjectStream) jaxbUnmarshaller.unmarshal( new File(getFileName(defInput)) );
         } catch (JAXBException e) {
             e.printStackTrace();
         }
@@ -137,7 +190,6 @@ public class Main {
         int last = 0;
         int size = logFile.size();
         Matcher matcher;
-        // TODO: Increase speed. Look at using parrallel worker threads.
         for (String s : logFile) {
             for(String p : regularExpressions.keySet()) {
                 matcher = regularExpressions.get(p).matcher(s);
@@ -158,17 +210,15 @@ public class Main {
         AtomicInteger last = new AtomicInteger();
         int size = logFile.size();
         final Matcher[] matcher = new Matcher[1];
-        // TODO: Increase speed. Look at using parrallel worker threads.
         logFile.parallelStream()
                 .forEach(s ->{
-                    regularExpressions.keySet().parallelStream()
-                            .forEach(p ->{
-                                matcher[0] = regularExpressions.get(p).matcher(s);
-                                if(matcher[0].find()) {
-                                    if(!errors.contains(p))
-                                        errors.add(p.toString());
-                                }
-                            });
+                    for(String p : regularExpressions.keySet()) {
+                        matcher[0] = regularExpressions.get(p).matcher(s);
+                        if(matcher[0].find()) {
+                            if(!errors.contains(p))
+                                errors.add(p.toString());
+                        }
+                    }
                     count.getAndIncrement();
                     last.set(printPercentage(count.get(), last.get(), size));
                 });
@@ -181,6 +231,29 @@ public class Main {
             print("\r      " + percentage + "%");
         }
         return percentage;
+    }
+
+    private static URL getUrl(String defInput) throws MalformedURLException {
+        if(isUrl(defInput)) {
+            return new URL(defInput);
+
+        } else {
+            return new URL(getDefinitionUrl(defInput));
+        }
+    }
+
+    private static String getFileName(String defInput) {
+        if(isUrl(defInput)) {
+            CRC32 crc = new CRC32();
+            crc.update(defInput.getBytes());
+            return String.valueOf(crc.getValue()) + ".xml";
+        } else {
+            return defInput + ".xml";
+        }
+    }
+
+    private static boolean isUrl(String input) {
+        return input.contains("http") || input.contains("https");
     }
 
     private static void print(CharSequence text) {
